71fe002b6c40f933f443e7f280016a0f8a639b53
pbeheyt <pbeheyt@debian-pbeheyt.pbeheyt>
2023-10-11 04:14:34 +0200

    mariadb bug at launch + wordpress script to fix

--- srcs/requirements/mariadb/tools/auto_config.sh
+++ srcs/requirements/mariadb/tools/auto_config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Start the MySQL service
 service mysql start;
 
--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,27 +1,35 @@
+#!/bin/bash
 sleep 10
 
-sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 
-cp wp-config-sample.php wp-config.php
+# cp wp-config-sample.php wp-config.php
 
-wp core install --allow-root\
-       --url=$DOMAIN_NAME\
-       --title=$SITE_TITLE\
-       --admin_user=$ADMIN_USER\
-       --admin_password=$ADMIN_PASSWORD\
-       --admin_email=$ADMIN_EMAIL\
+wp config create --allow-root \
+       --dbname=$MYSQL_DATABASE \
+	--dbuser=$MYSQL_USER \
+	--dbpass=$MYSQL_PASSWORD \
+	--dbhost=MYSQL_HOSTNAME:3306 \
        --path='/var/www/html'
 
-wp user create --allow-root
-       --role=author $USER1_LOGIN $USER1_EMAIL\
-       --user_pass=$USER1_PASSWORD\
-       --path='/var/www/html' >> log.txt
+wp core install --allow-root \
+    --url="$DOMAIN_NAME" \
+    --title="$SITE_TITLE" \
+    --admin_user="$ADMIN_USER" \
+    --admin_password="$ADMIN_PASSWORD" \
+    --admin_email="$ADMIN_EMAIL" \
+    --path='/var/www/html'
+
+wp user create --allow-root \
+    --role=author "$USER1_LOGIN" "$USER1_EMAIL" \
+    --user_pass="$USER1_PASSWORD" \
+    --path='/var/www/html' >> log.txt
 
 if [ ! -d /run/php ]; then
-    mkdir ./run/php
+    mkdir -p /run/php
 fi
 
 /usr/sbin/php-fpm7.3 -F
